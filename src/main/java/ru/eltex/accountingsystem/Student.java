104ac0d1a17d1d2766ee28e85e0479b3ae5ad9bd
klskvmr <koloskova.marii@gmail.com>
2019-08-19 16:47:46 +0700

    student&teacher

--- src/main/java/ru/eltex/accountingsystem/Student.java
+++ src/main/java/ru/eltex/accountingsystem/Student.java
@@ -1,8 +1,14 @@
 package ru.eltex.accountingsystem;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
+@Setter
+@AllArgsConstructor
 @Document(collection = "students")
 public class Student extends User{
-
+    Integer group;
 }
--- src/main/java/ru/eltex/accountingsystem/Teacher.java
+++ src/main/java/ru/eltex/accountingsystem/Teacher.java
@@ -1,7 +1,13 @@
 package ru.eltex.accountingsystem;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
+@Setter
+@AllArgsConstructor
 @Document(collection = "teachers")
 public class Teacher extends User {
     private String[] groups;

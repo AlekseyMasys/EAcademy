be6198c13ab43014391280d3331d103c09e8aab3
klskvmr <koloskova.marii@gmail.com>
2019-08-19 16:57:49 +0700

    Test Result add

--- src/main/java/ru/eltex/accountingsystem/Student.java
+++ src/main/java/ru/eltex/accountingsystem/Student.java
@@ -5,10 +5,13 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @Document(collection = "students")
 public class Student extends User{
     Integer group;
+    ArrayList<TestResult> tests;
 }

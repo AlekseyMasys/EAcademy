758a2fe91b9958725d2d2434535da17b67d8b8c5
Arishenk <arishenk1998@gmail.com>
2019-08-20 16:29:13 +0700

    Add tests in subject

--- src/main/java/ru/eltex/accountingsystem/documents/Subject.java
+++ src/main/java/ru/eltex/accountingsystem/documents/Subject.java
@@ -3,6 +3,7 @@ package ru.eltex.accountingsystem.documents;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import ru.eltex.testsystem.model.TestStructure;
 
 import java.util.ArrayList;
 
@@ -14,10 +15,12 @@ public class Subject {
     private String title;
     private ArrayList<Task> tasks;
     private ArrayList<Group> groups;
+    private ArrayList<TestStructure> tests;
 
-    public Subject(String title, ArrayList<Task> tasks, ArrayList<Group> groups) {
+    public Subject(String title, ArrayList<Task> tasks, ArrayList<Group> groups, ArrayList<TestStructure> tests) {
         this.title = title;
         this.tasks = tasks;
         this.groups = groups;
+        this.tests = tests;
     }
 }
--- src/main/java/ru/eltex/accountingsystem/documents/Task.java
+++ src/main/java/ru/eltex/accountingsystem/documents/Task.java
@@ -1,6 +1,5 @@
 package ru.eltex.accountingsystem.documents;
 
-
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;

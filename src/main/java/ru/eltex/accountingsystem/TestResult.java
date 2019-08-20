6195e04c311f3c86cf38f1fae49b61a4060724d9
VladimirT8520 <vladimir.trescov.n@gmail.com>
2019-08-20 11:08:00 +0700

    add constructorTestResult

--- src/main/java/ru/eltex/accountingsystem/TestResult.java
+++ src/main/java/ru/eltex/accountingsystem/TestResult.java
@@ -10,4 +10,9 @@ import lombok.Setter;
 public class TestResult {
     String testItem;
     Integer result;
+
+    public TestResult(String str, int i) {
+        String testItem;
+        Integer result;
+    }
 }

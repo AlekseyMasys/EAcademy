189bc418774ff27d606077a958e362ada8eaf1fe
Arishenk <arishenk1998@gmail.com>
2019-08-20 15:18:54 +0700

    Spit tasks

---
+++ src/main/java/ru/eltex/accountingsystem/documents/Task.java
@@ -0,0 +1,28 @@
+package ru.eltex.accountingsystem.documents;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document(collection = "task")
+public class Task {
+    @Id
+    private Integer id;
+    private String title;
+    private String description;
+    private Integer maxScores;
+    private String answer;
+    private Integer scores;
+
+    public Task(String title, String description, Integer maxScores, String answer, Integer scores){
+        this.title = title;
+        this.description = description;
+        this.maxScores = maxScores;
+        this.answer = answer;
+        this.scores = scores;
+    }
+}

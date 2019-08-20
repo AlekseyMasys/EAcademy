268e7068b682bdbc18ce939834e9d3fcb80386d1
klskvmr <koloskova.marii@gmail.com>
2019-08-20 16:08:50 +0700

    add Student

--- src/main/java/ru/eltex/accountingsystem/documents/Student.java
+++ src/main/java/ru/eltex/accountingsystem/documents/Student.java
@@ -2,26 +2,25 @@ package ru.eltex.accountingsystem.documents;
 
 import lombok.Getter;
 import lombok.Setter;
+
 import org.springframework.data.mongodb.core.mapping.Document;
 import ru.eltex.accountingsystem.enums.Role;
-import ru.eltex.accountingsystem.TestResult;
 import ru.eltex.accountingsystem.User;
 
+import javax.security.auth.Subject;
 import java.util.ArrayList;
 
 @Getter
 @Setter
 @Document(collection = "students")
 public class Student extends User {
-    Integer group;
-    ArrayList<TestResult> tests;
+    ArrayList<Subject> subjects;
 
     public Student(String login, String password,
                    String email, String fio, Role role,
-                   Integer group, ArrayList<TestResult> tests){
+                   ArrayList<Subject> subjects){
         super(login,password,email, fio, role);
 
-        this.group = group;
-        this.tests = tests;
+        this.subjects = subjects;
     }
 }

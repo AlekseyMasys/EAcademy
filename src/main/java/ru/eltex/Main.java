36906800ec9f685bab94933f40cc36f6b8181246
klskvmr <koloskova.marii@gmail.com>
2019-08-21 17:29:09 +0700

    Web

--- src/main/java/ru/eltex/Main.java
+++ src/main/java/ru/eltex/Main.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import ru.eltex.accountsystem.dao.StudentRepository;
+import ru.eltex.accountsystem.dao.TeacherRepository;
 import ru.eltex.accountsystem.enums.Role;
 import ru.eltex.accountsystem.model.Group;
 import ru.eltex.accountsystem.model.Subject;
 import ru.eltex.accountsystem.model.Task;
 import ru.eltex.accountsystem.model.users.Student;
+import ru.eltex.accountsystem.model.users.Teacher;
 import ru.eltex.testsystem.model.TestStructure;
 
 import java.util.ArrayList;
@@ -21,12 +23,20 @@ public class Main {
     }
 
     @Bean
-    public CommandLineRunner demo(StudentRepository studentRepository) {
+    public CommandLineRunner demo(StudentRepository studentRepository, TeacherRepository teacherRepository) {
         return (args) -> {
-            ArrayList<Subject>subjects = new ArrayList<>();
+
+
+            ArrayList<Subject> subjects = new ArrayList<>();
             subjects.add(new Subject("title", new ArrayList<Task>(), new ArrayList<Group>(), new ArrayList<TestStructure>()));
+            subjects.add(new Subject("math", new ArrayList<Task>(), new ArrayList<Group>(), new ArrayList<TestStructure>()));
+
+
             Student student = new Student("student", "password", "qwerty@mail.ru", "Alex", Role.STUDENT, subjects);
             studentRepository.save(student);
+
+            Teacher teacher = new Teacher("teacher", "password", "qwerty@mail.ru", "Bob", Role.TEACHER, subjects);
+            teacherRepository.save(teacher);
         };
     }
 }

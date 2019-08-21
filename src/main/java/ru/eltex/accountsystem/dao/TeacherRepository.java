e0a8c962c0fa277c4907fb90dd0fea0c21537354
klskvmr <koloskova.marii@gmail.com>
2019-08-21 12:21:46 +0700

    add Repositories

--- src/main/java/ru/eltex/accountsystem/dao/TeacherRepository.java
+++ src/main/java/ru/eltex/accountsystem/dao/TeacherRepository.java
@@ -2,8 +2,6 @@ package ru.eltex.accountsystem.dao;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import ru.eltex.accountsystem.model.users.Teacher;
-import java.util.List;
 
-public interface TeacherRepository extends MongoRepository<Teacher, Integer> {
-    List<Teacher> findById(String id);
+public interface TeacherRepository extends MongoRepository<Teacher, String> {
 }

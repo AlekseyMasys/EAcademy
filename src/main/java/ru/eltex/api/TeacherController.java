c75d14624b4ba60fcffdb2b0b29aa286f60c1481
Artem <Artem04.95@gmail.com>
2019-08-22 17:49:52 +0700

    Revert "Add subject service"
    This reverts commit 0217757b

--- src/main/java/ru/eltex/api/TeacherController.java
+++ src/main/java/ru/eltex/api/TeacherController.java
@@ -4,25 +4,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.eltex.accountsystem.model.Group;
 import ru.eltex.accountsystem.model.Subject;
-import ru.eltex.accountsystem.model.users.Teacher;
 import ru.eltex.accountsystem.service.GroupService;
-import ru.eltex.accountsystem.service.SubjectService;
 import ru.eltex.accountsystem.service.TeacherService;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class TeacherController {
     private final TeacherService teacherService;
     private final GroupService groupService;
-    private final SubjectService subjectService;
 
-    public TeacherController(TeacherService teacherService, GroupService groupService, SubjectService subjectService) {
+    public TeacherController(TeacherService teacherService, GroupService groupService) {
         this.teacherService = teacherService;
         this.groupService = groupService;
-        this.subjectService = subjectService;
     }
 
     @RequestMapping(value = "/teacher/{id}", method = RequestMethod.GET)
@@ -33,46 +25,26 @@ public class TeacherController {
 
     @RequestMapping(value = "/teacher/{id}/subjects", method = RequestMethod.GET)
     public String getTeacherSubjects(@PathVariable("id") String id, Model modelSubjects) {
-        modelSubjects.addAllAttributes(getSubjects(id));
-        return "teacherSubjects";
-    }
-
-    private List<Subject> getSubjects(String id) {
-        List<Subject> subjects = new ArrayList<>();
-        Teacher teacher = teacherService.getTeacher(id);
-        teacher.getSubjects()
-                .stream()
-                .filter(sub -> subjects.add(subjectService.getSubject(sub)))
-                .collect(Collectors.toList());
-        return subjects;
+       modelSubjects.addAllAttributes(teacherService.getTeacherSubjects(id));
+       return "teacherSubjects";
     }
 
     @RequestMapping(value = "/teacher/{id}/groups", method = RequestMethod.GET)
     public String getTeacherGroups(@PathVariable("id") String id, Model modelGroup) {
-        List<Group> groups = getGroups(id);
-        modelGroup.addAllAttributes(groups.stream().distinct().collect(Collectors.toList()));
+        modelGroup.addAllAttributes(teacherService.getTeacherGroups(id));
         return "groups";
-    }
-
-    private List<Group> getGroups(String id) {
-        List<Subject> subjects = getSubjects(id);
-        List<Group> groups = new ArrayList<>();
-        subjects.forEach(sub -> groups.addAll(sub.getGroups()));
-        return groups;
     }
 
     @RequestMapping(value = "/teacher/{id}/subject/{idSubject}", method = RequestMethod.GET)
     public String getSubjectGroups(@PathVariable("id") String id, @PathVariable("idSubject") String idSubject, Model modelGroup) {
-        Subject subject = subjectService.getSubject(idSubject);
-        modelGroup.addAllAttributes(subject.getGroups());
+        modelGroup.addAllAttributes(teacherService.getSubjectGroups(id, idSubject));
         return "groupsFromSubject";
     }
 
     @RequestMapping(value = "/getStudentsFromGroup/{id}/{idSubject}/{idGroup}", method = RequestMethod.GET)
     public String getStudentsFromGroup(@PathVariable("id") String id, @PathVariable("idGroup") String idGroup,
                                               @PathVariable String idSubject, Model modelStudents) {
-        List<Group> groups = getGroups(id);
-        modelStudents.addAllAttributes(groups.stream().filter(group -> group.getId().equals(idGroup)).findFirst().get().getStudents());
+        modelStudents.addAllAttributes(teacherService.getStudentsFromGroup(id, idGroup, idSubject));
         return "studentsFromGroup";
     }
 
@@ -90,7 +62,7 @@ public class TeacherController {
 
     @RequestMapping(value = "/addSubject", method = RequestMethod.POST)
     public void addSubject(@RequestBody Subject subject) {
-        subjectService.addSubject(subject);
+        teacherService.addSubject(subject);
     }
 
     @RequestMapping(value = "/addScores/{studentId}/{taskId}/{scores}/{status}", method = RequestMethod.POST)

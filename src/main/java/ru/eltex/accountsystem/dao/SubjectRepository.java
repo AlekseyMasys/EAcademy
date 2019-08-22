bab1c11ddbe21b2a48441954c3dd244df5270adc
ArinaNedobitkova <arishenk1998@gmail.com>
2019-08-22 15:47:38 +0700

    Migrate from rest to mvc

--- src/main/java/ru/eltex/accountsystem/dao/SubjectRepository.java
+++ src/main/java/ru/eltex/accountsystem/dao/SubjectRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import ru.eltex.accountsystem.model.Subject;
 @Repository
-public interface SubjectRepository extends MongoRepository<Subject, String>,SubjectRepositoryCustom {
+public interface SubjectRepository extends MongoRepository<Subject, String>, SubjectRepositoryCustom {
 }

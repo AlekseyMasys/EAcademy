f62346161cca9f373cd647c16ea77bca14435e1c
Aleksey Masys <masis.leha@mail.ru>
2019-08-23 14:59:08 +0700

    merge master2

--- src/main/java/ru/eltex/accountsystem/repository/TeacherRepository.java
+++ src/main/java/ru/eltex/accountsystem/repository/TeacherRepository.java
@@ -1,6 +1,7 @@
-package ru.eltex.accountsystem.dao;
+package ru.eltex.accountsystem.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import ru.eltex.accountsystem.dao.TeacherRepositoryCustom;
 import ru.eltex.accountsystem.model.users.Teacher;
 
 public interface TeacherRepository extends MongoRepository<Teacher, String>, TeacherRepositoryCustom {

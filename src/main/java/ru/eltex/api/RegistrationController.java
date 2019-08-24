24377fa92514522dd378bf7c44694ff12f78b377
Aleksey Masys <masis.leha@mail.ru>
2019-08-24 18:12:53 +0700

    rename UserController to RegistrationController

--- src/main/java/ru/eltex/api/RegistrationController.java
+++ src/main/java/ru/eltex/api/RegistrationController.java
@@ -3,20 +3,18 @@ package ru.eltex.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.eltex.accountsystem.model.User;
-import ru.eltex.accountsystem.model.UserRole;
 import ru.eltex.accountsystem.service.UserRegistrationService;
 
 @Controller
-public class UserController {
+public class RegistrationController {
 
     private final UserRegistrationService userRegistrationService;
 
     @Autowired
-    public UserController(UserRegistrationService userRegistrationService) {
+    public RegistrationController(UserRegistrationService userRegistrationService) {
         this.userRegistrationService = userRegistrationService;
     }
 

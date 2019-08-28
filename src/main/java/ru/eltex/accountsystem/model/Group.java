64b2271f490f932e35f1c9d93e6d8d7b14a3f0cf
klskvmr <koloskova.marii@gmail.com>
2019-08-28 14:08:44 +0700

    nuclear refactoring

--- src/main/java/ru/eltex/accountsystem/model/Group.java
+++ src/main/java/ru/eltex/accountsystem/model/Group.java
@@ -3,33 +3,32 @@ package ru.eltex.accountsystem.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.codehaus.jackson.JsonNode;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import ru.eltex.accountsystem.model.users.Student;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Document(collection = "groups")
 public class Group {
-    @Id
     private String id;
     private String title;
     private String table;
-    private ArrayList<Student> students;
+    private ArrayList<String> studentIds;
 
-    public Group(String title, ArrayList<Student> students) {
+    public Group(String title, ArrayList<String> studentIds) {
+        id = UUID.randomUUID().toString();
         this.title = title;
-        this.students = students;
+        this.studentIds = studentIds;
     }
 
-    public Group(String title, String table, ArrayList<Student> students) {
+    public Group(String title, String table, ArrayList<String> studentIds) {
+        id = UUID.randomUUID().toString();
         this.title = title;
         this.table = table;
-        this.students = students;
+        this.studentIds = studentIds;
     }
 
     public void setTable(String table) {
